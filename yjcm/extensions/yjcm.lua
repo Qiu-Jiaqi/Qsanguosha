ad15bef74213ecaa584378f5f839bb02d3b679f5
Qiu-Jiaqi <qiu_jiaqi@qq.com>
2022-02-02 22:18:38 +0800

    修改技能：绝情（张春华）

--- yjcm/extensions/yjcm.lua
+++ yjcm/extensions/yjcm.lua
@@ -1,12 +1,12 @@
 extension = sgs.Package("yjcm", sgs.Package_GeneralPack)
 -- 张春华
 zhangchunhua = sgs.General(extension, "zhangchunhua", "wei", "3", false, true)
--- 绝情：锁定技，你即将造成的伤害视为失去体力。
+-- 绝情：锁定技，你即将造成或受到的伤害均视为失去体力，此时若你手牌不为空，需弃置一张手牌。
 jueqing =
     sgs.CreateTriggerSkill {
     name = "jueqing",
     frequency = sgs.Skill_Compulsory,
-    events = {sgs.Predamage},
+    events = {sgs.Predamage, sgs.DamageForseen},
     can_trigger = function(self, event, room, player, data)
         if player and player:isAlive() and player:hasSkill(self:objectName()) then
             return self:objectName()
@@ -22,6 +22,9 @@ jueqing =
         return false
     end,
     on_effect = function(self, event, room, player, data, ask_who)
+        if not player:isKongcheng() then
+            room:askForDiscard(player, self:objectName(), 1, 1, true, false)
+        end
         room:loseHp(data:toDamage().to, data:toDamage().damage)
         -- 取消原效果，改为体力流失，返回true
         return true
@@ -66,7 +69,7 @@ sgs.LoadTranslationTable {
     ["#zhangchunhua"] = "冷血皇后",
     ["~zhangchunhua"] = "今夕何夕，君已陌路。",
     ["jueqing"] = "绝情",
-    [":jueqing"] = "锁定技，你即将造成的伤害视为失去体力。 ",
+    [":jueqing"] = "锁定技，你即将造成或受到的伤害均视为失去体力，此时若你手牌不为空，需弃置一张手牌。",
     ["$jueqing1"] = "化爱为恨，恨之透骨。",
     ["$jueqing2"] = "为上位者，当至无情。",
     ["shangshi"] = "伤逝",
