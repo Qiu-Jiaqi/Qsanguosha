472edf70aec55e32d9a28215359b2b6448ce4f1a
Qiu-Jiaqi <qiu_jiaqi@qq.com>
2022-02-03 19:22:15 +0800

    新增武将：张星彩

--- sp/extensions/sp.lua
+++ sp/extensions/sp.lua
@@ -1,4 +1,7 @@
 extension = sgs.Package("sp", sgs.Package_GeneralPack)
+sgs.LoadTranslationTable {
+    ["sp"] = "sp"
+}
 -- 戏志才
 xizhicai = sgs.General(extension, "xizhicai", "wei", "3", true, true)
 -- 天妒：当你的判定结果确定后，你可获得判定牌。
@@ -153,7 +156,6 @@ sgs.insertRelatedSkills(extension, xianfu, xianfu_target)
 -- extension:insertRelatedSkills("xianfu", "#xianfu_target")
 xizhicai:addSkill(chouce)
 sgs.LoadTranslationTable {
-    ["sp"] = "sp",
     ["xizhicai"] = "戏志才",
     ["&xizhicai"] = "戏志才",
     ["#xizhicai"] = "负俗的天才",
@@ -164,16 +166,203 @@ sgs.LoadTranslationTable {
     ["$tiandu_xizhicai1"] = "既是如此~",
     ["$tiandu_xizhicai2"] = "天意，不可逆~",
     ["xianfu"] = "先辅",
-    [":xianfu"] = "锁定技，游戏开始时，你选择一名其他角色，当其受到伤害后，你受到等量的伤害，当其回复体力后，你回复等量的体力。 ",
+    [":xianfu"] = "锁定技，游戏开始时，你选择一名其他角色，当其受到伤害后，你受到等量的伤害，当其回复体力后，你回复等量的体力。",
     ["$xianfu1"] = "辅佐明君，从一而终。",
     ["$xianfu2"] = "吾于此生，竭尽所能。",
     ["@xianfu_choose"] = "发动“先辅”，选择一名其他角色。",
     ["@xianfu"] = "先辅",
     ["chouce"] = "筹策",
-    [":chouce"] = "当你受到1点伤害后，你可以判定，若结果为：黑色，你弃置一名角色区域里的一张牌；红色，你令一名角色摸一张牌（先辅的角色摸两张）。 ",
+    [":chouce"] = "当你受到1点伤害后，你可以判定，若结果为：黑色，你弃置一名角色区域里的一张牌；红色，你令一名角色摸一张牌（先辅的角色摸两张）。",
     ["$chouce1"] = "一筹一划，一策一略。",
     ["$chouce2"] = "主公之忧，吾之所思也。",
     ["@chouce_draw"] = "你可以发动“筹策”，选择一名角色，令其摸一张牌（先辅的角色摸两张）。",
     ["@chouce_discard"] = "你可以发动“筹策”，选择一名角色，弃置其区域里的一张牌。"
+}
+-- 张星彩
+zhangxingcai = sgs.General(extension, "zhangxingcai", "shu", "3", false, true)
+-- 甚贤：每当一名其他角色于你的回合外因弃置而失去基本牌后，你可以摸一张牌。
+--       我吐了，整了一早上，反正我是吐了，卡牌移动原因的结构体，什么鬼作用都没有的，用不了，一早上白白浪费时间
+-- shenxian =
+--     sgs.CreateTriggerSkill {
+--     name = "shenxian",
+--     events = {sgs.BeforeCardsMove},
+--     can_trigger = function(self, event, room, player, data)
+--         local zhangxingcai = room:findPlayerBySkillName(self:objectName())
+--         if zhangxingcai and zhangxingcai:isAlive() and zhangxingcai:hasSkill(self:objectName()) then
+--             local move = data:toMoveOneTime()
+--             if move.from and move.from:objectName() ~= zhangxingcai:objectName() then
+--                 return ""
+--             end
+--             --     zhangxingcai ~= player and
+--             --     zhangxingcai:getPhase() == sgs.Player_NotActive and
+--             if
+--                 bit32.band(move.reason.m_reason, sgs.CardMoveReason_S_MASK_BASIC_REASON) ==
+--                     sgs.CardMoveReason_S_REASON_DISCARD
+--              then
+--                 -- local reason = bit32.band(move.reason.m_reason, sgs.CardMoveReason_S_MASK_BASIC_REASON)
+--                 for _, id in sgs.qlist(move.card_ids) do
+--                     if sgs.Sanguosha:getCard(id):isKindOf("BasicCard") then
+--                         return self:objectName(), zhangxingcai
+--                     end
+--                 end
+--             end
+--         end
+--         return ""
+--     end,
+--     on_cost = function(self, event, room, player, data, ask_who)
+--         if ask_who:askForSkillInvoke(self:objectName(), data) then
+--             room:broadcastSkillInvoke(self:objectName())
+--             return true
+--         end
+--         return false
+--     end,
+--     on_effect = function(self, event, room, player, data, ask_who)
+--         ask_who:drawCards(1, self:objectName())
+--         return false
+--     end
+-- }
+-- 放弃了，改技能呗
+-- 甚贤：每当一名其他角色在其回合内造成过伤害，你可以摸一张牌。
+shenxian =
+    sgs.CreateTriggerSkill {
+    name = "shenxian",
+    events = {sgs.Damage, sgs.EventPhaseEnd},
+    can_trigger = function(self, event, room, player, data)
+        local zhangxingcai = room:findPlayerBySkillName(self:objectName())
+        if zhangxingcai and zhangxingcai:isAlive() and zhangxingcai:hasSkill(self:objectName()) then
+            if player ~= zhangxingcai and event == sgs.Damage then
+                room:setPlayerFlag(player, self:objectName())
+            elseif
+                event == sgs.EventPhaseEnd and player:getPhase() == sgs.Player_Finish and
+                    player:hasFlag(self:objectName())
+             then
+                return self:objectName(), zhangxingcai
+            end
+        end
+        return ""
+    end,
+    on_cost = function(self, event, room, player, data, ask_who)
+        if ask_who:askForSkillInvoke(self:objectName(), data) then
+            room:broadcastSkillInvoke(self:objectName())
+            return true
+        end
+        return false
+    end,
+    on_effect = function(self, event, room, player, data, ask_who)
+        ask_who:drawCards(1, self:objectName())
+        return false
+    end
+}
+-- 枪舞：出牌阶段限一次，你可以进行判定：若如此做，直到下回合开始前，你使用点数小于判定牌点数的【杀】无距离限制，
+--      你使用点数大于判定牌点数的【杀】无次数限制且不计入次数限制，你使用点数等于判定牌点数的【杀】同时具备以上两点且伤害+1。
+qiangwu_card =
+    sgs.CreateSkillCard {
+    name = "qiangwu",
+    target_fixed = true,
+    on_use = function(self, room, source, targets)
+        local judge = sgs.JudgeStruct()
+        judge.who = source
+        judge.reason = self:objectName()
+        judge.play_animation = true
+        room:judge(judge)
+        room:setPlayerMark(source, self:objectName(), judge.card:getNumber())
+    end
+}
+qiangwu_vs =
+    sgs.CreateZeroCardViewAsSkill {
+    name = "qiangwu",
+    view_as = function(self)
+        local card = qiangwu_card:clone()
+        card:setSkillName(self:objectName())
+        card:setShowSkill(self:objectName())
+        return card
+    end,
+    enabled_at_play = function(self, player)
+        return not player:hasUsed("#qiangwu")
+    end
+}
+qiangwu =
+    sgs.CreateTriggerSkill {
+    name = "qiangwu",
+    view_as_skill = qiangwu_vs,
+    events = {sgs.EventPhaseStart, sgs.PreCardUsed, sgs.ConfirmDamage},
+    can_trigger = function(self, event, room, player, data)
+        -- 只有进行了判定，设置了mark，才有必要进行回合结束mark置零、使用杀时判断等操作
+        if player and player:isAlive() and player:hasSkill(self:objectName()) and player:getMark(self:objectName()) ~= 0 then
+            return self:objectName()
+        end
+        return ""
+    end,
+    on_cost = function(self, event, room, player, data, ask_who)
+        return player:hasShownSkill(self:objectName()) and true or false
+    end,
+    on_effect = function(self, event, room, player, data, ask_who)
+        if event == sgs.EventPhaseStart and player:getPhase() == sgs.Player_NotActive then
+            -- 回合结束时，mark置零
+            room:setPlayerMark(player, self:objectName(), 0)
+        elseif event == sgs.PreCardUsed then
+            local use = data:toCardUse()
+            if use.card and use.card:isKindOf("Slash") and use.card:getNumber() >= player:getMark(self:objectName()) then
+                -- 不计入次数限制
+                room:addPlayerHistory(player, use.card:getClassName(), -1)
+                use.m_addHistory = false
+                data:setValue(use)
+            end
+        elseif event == sgs.ConfirmDamage then
+            local damage = data:toDamage()
+            if damage.card:isKindOf("Slash") and damage.card:getNumber() == player:getMark(self:objectName()) then
+                -- 伤害值+1
+                damage.damage = damage.damage + 1
+                data:setValue(damage)
+                local msg = sgs.LogMessage()
+                msg.type = "$qiangwu_addDamage"
+                msg.from = player
+                msg.arg = "qiangwu"
+                msg.card_str = damage.card:toString()
+                msg.arg2 = "+1"
+                room:sendLog(msg)
+            end
+        end
+        return false
+    end
+}
+qiangwu_mod =
+    sgs.CreateTargetModSkill {
+    name = "#qiangwu_mod",
+    distance_limit_func = function(self, player, card)
+        local n = player:getMark("qiangwu")
+        if n ~= 0 and card:getNumber() ~= 0 and card:getNumber() <= n then
+            return 732
+        end
+        return 0
+    end
+    -- 用History才能实现技能中描述的不计次数限制，用这种的话，先用了>=的牌，再用<的则不可以，就是计入了次数了
+    -- residue_func = function(self, player, card)
+    --     local n = player:getMark("qiangwu")
+    --     if n ~= 0 and card:getNumber() ~= 0 and card:getNumber() >= n then
+    --         return 732
+    --     end
+    --     return 0
+    -- end
+}
+zhangxingcai:addSkill(shenxian)
+zhangxingcai:addSkill(qiangwu)
+zhangxingcai:addSkill(qiangwu_mod)
+sgs.insertRelatedSkills(extension, qiangwu, qiangwu_mod)
+sgs.LoadTranslationTable {
+    ["zhangxingcai"] = "张星彩",
+    ["&zhangxingcai"] = "张星彩",
+    ["#zhangxingcai"] = "敬哀皇后",
+    ["~zhangxingcai"] = "复兴汉室之路，臣妾再也不能陪伴左右。",
+    ["shenxian"] = "甚贤",
+    -- [":shenxian"] = "每当一名其他角色于你的回合外因弃置而失去基本牌后，你可以摸一张牌。",
+    [":shenxian"] = "每当一名其他角色在其回合内造成过伤害，你可以摸一张牌。",
+    ["$shenxian1"] = "抚慰军心，以安国事。",
+    ["$shenxian2"] = "愿尽己力，为君分忧。",
+    ["qiangwu"] = "枪舞",
+    [":qiangwu"] = "出牌阶段限一次，你可以进行判定：若如此做，本回合，你使用点数小于判定牌点数的【杀】无距离限制，你使用点数大于判定牌点数的【杀】无次数限制且不计入次数限制，你使用点数等于判定牌点数的【杀】同时具备以上两点且伤害+1。",
+    ["$qiangwu1"] = "咆哮沙场，万夫不敌！",
+    ["$qiangwu2"] = "父亲未竟之业，由我继续！",
+    ["$qiangwu_addDamage"] = "%from 执行了“%arg”的效果，%card 伤害值 %arg2"
 }
 return {extension}
