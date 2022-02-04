570252d31294062a3e60c742ce5c2db25dffb944
Qiu-Jiaqi <qiu_jiaqi@qq.com>
2022-02-04 11:56:46 +0800

    修复了七弦不亮将也能生效的bug，优化了神赋的触发选择（神甄姬）

--- deity/extensions/deity.lua
+++ deity/extensions/deity.lua
@@ -13,19 +13,14 @@ shenfu =
         return ""
     end,
     on_cost = function(self, event, room, player, data, ask_who)
-        if player:askForSkillInvoke(self:objectName()) then
-            return true
-        end
-        return false
-    end,
-    on_phasechange = function(self, player)
-        local room = player:getRoom()
         local targets = room:getAlivePlayers()
         if player:getHandcardNum() % 2 == 0 then
             while true do
                 -- 询问选择目标
                 local target = room:askForPlayerChosen(player, targets, self:objectName(), "@shenfu_ou", true, true)
                 if target then
+                    -- 亮将
+                    player:showSkill(self:objectName())
                     room:broadcastSkillInvoke(self:objectName())
                     local choices = {"draw"}
                     if player:canDiscard(target, "h") then
@@ -46,6 +41,8 @@ shenfu =
                     if target:getHandcardNum() ~= target:getHp() then
                         break
                     end
+                else
+                    break
                 end
             end
         else
@@ -55,16 +52,23 @@ shenfu =
                 -- 询问选择目标
                 local target = room:askForPlayerChosen(player, targets, self:objectName(), "@shenfu_ji", true, true)
                 if target then
+                    -- 亮将
+                    player:showSkill(self:objectName())
                     room:broadcastSkillInvoke(self:objectName())
                     room:damage(sgs.DamageStruct(self:objectName(), player, target, 1, sgs.DamageStruct_Thunder))
-                    -- 移除死亡目标
+                    -- 移除指定过的目标
                     targets:removeOne(target)
                     if target:isAlive() then
                         break
                     end
+                else
+                    break
                 end
             end
         end
+        return false
+    end,
+    on_phasechange = function(self, player)
         return false
     end
 }
@@ -73,11 +77,36 @@ qixian =
     sgs.CreateMaxCardsSkill {
     name = "qixian",
     fixed_func = function(self, player)
-        return player:hasSkill(self:objectName()) and 7 or -1
+        return player:hasShownSkill(self:objectName()) and 7 or -1
+    end
+}
+qixian_trigger =
+    sgs.CreatePhaseChangeSkill {
+    name = "#qixian",
+    frequency = sgs.Skill_Compulsory,
+    can_trigger = function(self, event, room, player, data)
+        if
+            player and player:isAlive() and player:hasSkill(self:objectName()) and
+                not player:hasShownSkill(self:objectName()) and
+                player:getPhase() == sgs.Player_Discard and
+                player:getHandcardNum() > player:getHp()
+         then
+            return self:objectName()
+        end
+        return ""
+    end,
+    on_cost = function(self, event, room, player, data, ask_who)
+        return player:askForSkillInvoke(self:objectName(), data) and true or false
+    end,
+    on_phasechange = function(self, player)
+        player:showSkill("qixian")
+        return false
     end
 }
 shenzhenji:addSkill(shenfu)
 shenzhenji:addSkill(qixian)
+shenzhenji:addSkill(qixian_trigger)
+sgs.insertRelatedSkills(extension, qixian, qixian_trigger)
 sgs.LoadTranslationTable {
     ["deity"] = "神包",
     ["shenzhenji"] = "神甄姬",
@@ -93,6 +122,7 @@ sgs.LoadTranslationTable {
     ["$shenfu1"] = "河洛之神，诗赋可抒。",
     ["$shenfu2"] = "云神鱼游，罗扇掩面。",
     ["qixian"] = "七弦",
+    ["#qixian"] = "七弦",
     [":qixian"] = "锁定技，你的手牌上限为7。"
 }
 return {extension}
