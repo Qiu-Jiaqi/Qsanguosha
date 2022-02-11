9a23295c8bfcc289fa7931849d58019d75a6582a
Qiu-Jiaqi <qiu_jiaqi@qq.com>
2022-02-11 21:02:39 +0800

    修改沮授技能渐营

--- yjcm4/extensions/yjcm4.lua
+++ yjcm4/extensions/yjcm4.lua
@@ -4,32 +4,83 @@ sgs.LoadTranslationTable {
 }
 -- 沮授，与据守拼音一致，改为我用的自然码双拼命名
 juub = sgs.General(yjcm4, "juub", "qun", "3", true, true)
--- 渐营：每当你使用（不包括响应）一张牌时，若此牌与你使用的上一张牌花色或点数相同，你可以摸一张牌。
+-- 渐营：每当你使用（包括响应）一张牌时，若此牌与你使用的上一张牌花色或点数相同，你可以摸一张牌。
+-- 出牌阶段限一次，你可以将一张牌当做任意一张基本牌使用，且该牌使用不计入次数限制，若你使用的上一张牌有花色，则此牌的花色视为与上一张牌的花色相同，否则花色不变。
+jianying_vs =
+    sgs.CreateOneCardViewAsSkill {
+    name = "jianying",
+    filter_pattern = ".",
+    view_as = function(self, card)
+        local last = sgs.Self:getTag("jianying_card"):toCard()
+        local suit = card:getSuit()
+        -- 若上一张牌有花色
+        if last and last:getSuit() ~= sgs.Card_NoSuit and last:getSuit() ~= sgs.Card_SuitToBeDecided then
+            suit = last:getSuit()
+        end
+        local new_card = sgs.Sanguosha:cloneCard(sgs.Self:getTag(self:objectName()):toString(), suit, card:getNumber())
+        new_card:addSubcard(card)
+        new_card:setSkillName(self:objectName())
+        new_card:setShowSkill(self:objectName())
+        -- 记录本回合已发动
+        sgs.Self:setFlags(self:objectName())
+        return new_card
+    end,
+    enabled_at_play = function(self, player)
+        return not sgs.Self:hasFlag(self:objectName())
+    end
+}
 jianying =
     sgs.CreateTriggerSkill {
     name = "jianying",
-    events = sgs.CardUsed,
+    view_as_skill = jianying_vs,
+    guhuo_type = "b",
+    events = {sgs.CardUsed, sgs.CardResponded, sgs.PreCardUsed},
+    on_record = function(self, event, room, player, data)
+        if
+            event == sgs.PreCardUsed and player and player:isAlive() and player:hasSkill(self:objectName()) and
+                data:toCardUse().card:getSkillName() == self:objectName()
+         then
+            -- 不计入次数限制
+            local use = data:toCardUse()
+            room:addPlayerHistory(player, use.card:getClassName(), -1)
+            use.m_addHistory = false
+            data:setValue(use)
+        end
+    end,
     can_trigger = function(self, event, room, player, data)
         -- 当前使用的牌
-        local card = data:toCardUse().card
+        local card = event == sgs.CardResponded and data:toCardResponse().m_card or data:toCardUse().card
         if
-            player and player:isAlive() and player:hasSkill(self:objectName()) and data:toCardUse().from == player and
-                card:getTypeId() ~= sgs.Card_TypeSkill and
-                card:getNumber() ~= 0 and
-                card:getSuit() ~= sgs.Card_NoSuit
+            -- 注意：事件更改时，这里需要改，这里应该是包括sgs.CardUsed和sgs.CardResponded
+            event ~= sgs.PreCardUsed and player and player:isAlive() and player:hasSkill(self:objectName()) and
+                -- 这个判断就是多余的，player拥有技能就肯定能发动了
+                -- data:toCardUse().from == player and
+                card and
+                card:getTypeId() ~= sgs.Card_TypeSkill
          then
+            -- 这张牌有无花色、点数都要存
+            -- card:getNumber() ~= 0 and
+            -- card:getSuit() ~= sgs.Card_NoSuit and
+            -- card:getSuit() ~= sgs.Card_SuitToBeDecided
             -- 上次使用的牌
-            local last = room:getTag("jianying_card"):toCard()
+            local last = sgs.Self:getTag("jianying_card"):toCard()
             -- 存储当前使用的牌
             local card_data = sgs.QVariant()
             card_data:setValue(card)
-            room:setTag("jianying_card", card_data)
+            sgs.Self:setTag("jianying_card", card_data)
             -- local msg = sgs.LogMessage()
             -- msg.type = "$test_jianying"
             -- msg.arg = card:getNumber()
             -- room:sendLog(msg)
             -- msg.arg = card:getSuitString()
             -- room:sendLog(msg)
+            -- 花色、点数不为空在这里判断才对，若花色、点数都为空，则肯定不能，有一个不为空则可与上一张牌比较
+            if
+                card:getNumber() == 0 and
+                    (card:getSuit() == sgs.Card_NoSuit or card:getSuit() == sgs.Card_SuitToBeDecided)
+             then
+                return ""
+            end
             if last and (card:getNumber() == last:getNumber() or card:getSuit() == last:getSuit()) then
                 return self:objectName()
             end
@@ -100,9 +151,8 @@ sgs.LoadTranslationTable {
     ["&juub"] = "沮授",
     ["#juub"] = "监军谋国",
     ["~juub"] = "志士凋亡，河北哀矣。",
-    -- ["$test_jianying"] = "%arg test",
     ["jianying"] = "渐营",
-    [":jianying"] = "每当你使用（不包括响应）一张牌时，若此牌与你使用的上一张牌花色或点数相同，你可以摸一张牌。",
+    [":jianying"] = "每当你使用（包括响应）一张牌时，若此牌与你使用的上一张牌花色或点数相同，你可以摸一张牌。出牌阶段限一次，你可以将一张牌当做任意一张基本牌使用，且该牌使用不计入次数限制，若你使用的上一张牌有花色，则此牌的花色视为与上一张牌的花色相同，否则花色不变。",
     ["$jianying1"] = "由缓至急，循循而进。",
     ["$jianying2"] = "事须缓图，欲速不达也。",
     ["shibei"] = "矢北",
